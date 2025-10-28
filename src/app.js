74521a5142984bbe67551094113c12bdde37a947
ramzihaj <ramzi2020haj@gmail.com>
2025-10-28 16:59:35 +0100

    setup Logger & Middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
 import express from 'express';
 import 'dotenv/config';
-
+import logger from '#config/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 const app = express();
 
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(
+  morgan('combined', {
+    stream: { write: message => logger.info(message.trim()) },
+  })
+);
+
 app.get('/', (req, res) => {
+  logger.info('Hello from Acuisitions!');
   res.status(200).send('Hello from Acquisitions!');
 });
 
